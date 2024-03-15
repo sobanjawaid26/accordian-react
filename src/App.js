fd4d9bbc9c0126ec7cb010be98ec75bddaf3dbf4
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-03-15 15:43:14 +0530

    App.js and Question.js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './App.css';
-
+import data from './data';
+import Question from './Question';
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+          return (
+            <Question key={question.id} {...question}></Question>
+          )
+        })}
+        </section>
+      </div>
+    </main>
   );
 }
 
